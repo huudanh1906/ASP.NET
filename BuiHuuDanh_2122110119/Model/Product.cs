2068a94c199b9e185e77a4c9263f7523b56711ad
unknown <doanhbui.5207@gmail.com>
2025-03-29 08:22:28 +0700

    first commit

---
+++ BuiHuuDanh_2122110119/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace BuiHuuDanh_2122110119.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+    }
+}
